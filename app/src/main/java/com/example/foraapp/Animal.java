29856a0c3ecc0ff11b9ced7e5a7730581618e089
Sfil0002 <sebastianfuzy388@gmail.com>
2022-03-14 20:07:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/foraapp/Animal.java
@@ -0,0 +1,114 @@
+package com.example.foraapp;
+
+import androidx.annotation.NonNull;
+
+public class Animal
+{
+    private String animal_name;
+    private String file_number;
+    private String breed;
+    private String sex;
+    private int approx_age;
+    private String photo_url;
+    private String description;
+
+    public Animal() {
+    }
+
+    public Animal(String animal_name, String file_number, String breed, String sex, int approx_age)
+    {
+        this.animal_name = animal_name;
+        this.file_number = file_number;
+        this.breed = breed;
+        this.sex = sex;
+        this.approx_age = approx_age;
+    }
+
+    public Animal(String animal_name, String file_number, String breed, String sex, int approx_age, String photo_url, String description)
+    {
+        this.animal_name = animal_name;
+        this.file_number = file_number;
+        this.breed = breed;
+        this.sex = sex;
+        this.approx_age = approx_age;
+        this.photo_url = photo_url;
+        this.description = description;
+    }
+
+    public String getAnimal_name() {
+        return animal_name;
+    }
+
+    public void setAnimal_name(String animal_name) {
+        this.animal_name = animal_name;
+    }
+
+    public String getFile_number() {
+        return file_number;
+    }
+
+    public void setFile_number(String file_number) {
+        this.file_number = file_number;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getApprox_age() {
+        return approx_age;
+    }
+
+    public void setApprox_age(int approx_age) {
+        this.approx_age = approx_age;
+    }
+
+    public String getPhoto_url() {
+        return photo_url;
+    }
+
+    public void setPhoto_url(String photo_url) {
+        this.photo_url = photo_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @NonNull
+    @Override
+    public String toString()
+    {
+        return "Name: " + animal_name + '\n' +
+                "File number: " + file_number + '\n' +
+                "Breed: " + breed + '\n' +
+                "Sex: " + sex + '\n' +
+                "Approximate age: " + approx_age +'\n'+
+                "Photo URL: " + photo_url + '\n' +
+                "Description: " + description + '\n';
+
+
+    }
+
+
+
+
+
+}
+
--- app/src/main/java/com/example/foraapp/addFragment.java
+++ app/src/main/java/com/example/foraapp/addFragment.java
@@ -4,61 +4,85 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link addFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class addFragment extends Fragment {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class addFragment extends Fragment
+{
+    private String animal_name, file_number, breed, sex, photo_url, description;
+    private int approx_age;
+    private EditText name_ET, file_number_ET, breed_ET, sex_ET,age_ET, photo_url_ET, description_ET;
+    private FirebaseDatabase database =FirebaseDatabase.getInstance("https://fora-app-ed8dc-default-rtdb.europe-west1.firebasedatabase.app/");
+    private DatabaseReference FORA_Database = database.getReference("Animal");
+    private Animal animal = new Animal();
 
-    public addFragment() {
+
+    public addFragment()
+    {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment addFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static addFragment newInstance(String param1, String param2) {
+
+
+
+    public static addFragment newInstance(String param1, String param2)
+    {
         addFragment fragment = new addFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+        Button addbutton = view.findViewById(R.id.addAnimal_button);
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name_ET = view.findViewById(R.id.name_ET);
+                file_number_ET = view.findViewById(R.id.filenumber_ET);
+                breed_ET = view.findViewById(R.id.breed_ET);
+                sex_ET = view.findViewById(R.id.sex_ET);
+                age_ET = view.findViewById(R.id.age_ET);
+                photo_url_ET = view.findViewById(R.id.photoUrl_ET);
+                description_ET = view.findViewById(R.id.des_ET);
+
+                animal_name = name_ET.getText().toString();
+                file_number = file_number_ET.getText().toString();
+                breed = breed_ET.getText().toString();
+                sex = sex_ET.getText().toString();
+                approx_age= Integer.parseInt(age_ET.getText().toString());
+                photo_url = photo_url_ET.getText().toString();
+                description = description_ET.getText().toString();
+
+                if(!TextUtils.isEmpty(animal_name) && !TextUtils.isEmpty(file_number) && !TextUtils.isEmpty(breed) && !TextUtils.isEmpty(sex) && !TextUtils.isEmpty(String.valueOf(approx_age)))
+                {
+
+                }
+                else
+                {
+
+                }
+
+            }
+        });
+
+        return view;
     }
 }

8d1635437f3af06c0ebce31a938bc0b915f2c049
Sfil0002 <sebastianfuzy388@gmail.com>
2022-05-26 19:35:28 +0200

    Initial commit

--- app/src/main/java/com/example/foraapp/qrGen.java
+++ app/src/main/java/com/example/foraapp/qrGen.java
@@ -1,9 +1,13 @@
 package com.example.foraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,10 +18,13 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class qrGen extends AppCompatActivity {
 
         EditText editText;
-        Button genbutton;
+        Button genbutton, saveImage;
         ImageView imageView;
 
         @Override
@@ -27,8 +34,10 @@ public class qrGen extends AppCompatActivity {
 
             editText = findViewById(R.id.edittext);
             genbutton = findViewById(R.id.genbutton);
-            imageView = findViewById(R.id.imageview);
 
+            imageView = findViewById(R.id.imageview);
+            ActivityCompat.requestPermissions(qrGen.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1 );
+            ActivityCompat.requestPermissions(qrGen.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1 );
             genbutton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -46,6 +55,5 @@ public class qrGen extends AppCompatActivity {
             });
 
         }
-
 
     }
